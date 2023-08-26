df339c0d3b7604f0e721f83ca3ed57f3d3614624
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2023-08-26 23:15:33 +0800

    Update wordsmithAPI.py

--- wordsmithAPI.py
+++ wordsmithAPI.py
@@ -1,24 +1,23 @@
-from fastapi import FastAPI, Request, BackgroundTasks
-from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, StreamingResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import PlainTextResponse
 import subprocess
 import asyncio
-import threading
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/templates", StaticFiles(directory="templates"), name="static")  # serve pics
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-def stream_subprocess_output(process, queue):
-    for line in iter(process.stdout.readline, ''):
-        queue.put(line)
-    process.stdout.close()
 
 @app.post("/execute")
-def execute_code(background_tasks: BackgroundTasks):
+def execute_code():
     process = subprocess.Popen(
         ['python', '/workspaces/GPT-Helper/privateGPT/privateGPT.py'],
         stdout=subprocess.PIPE,
@@ -27,15 +26,26 @@ def execute_code(background_tasks: BackgroundTasks):
         universal_newlines=True
     )
 
-    output_queue = asyncio.Queue()
-    background_tasks.add_task(stream_subprocess_output, process, output_queue)
-
     async def generate():
         while process.poll() is None:
-            try:
-                line = await asyncio.wait_for(output_queue.get(), timeout=1.0)
-                yield line
-            except asyncio.TimeoutError:
+            line = process.stdout.readline()
+            if not line:
+                await asyncio.sleep(0.1)  # Wait if no data is available
                 continue
+            yield line
+
+        # Process has exited, yield any remaining output
+        for line in process.stdout:
+            yield line
 
     return StreamingResponse(generate())
+
+
+@app.get("/get_bot_responses")
+def get_bot_responses():
+    try:
+        with open("bot_responses.txt", "r") as file:
+            responses = file.read()
+            return PlainTextResponse(content=responses) #read as text
+    except FileNotFoundError:
+        return JSONResponse(content={"error": "bot_responses.txt not found"}, status_code=404)
