a60c7869784099949ca38627a8f2a27071d9def6
Vince Versailles <50725388+elsaversailles@users.noreply.github.com>
2023-08-28 19:50:23 +0800

    Update wordsmithAPI.py
    Update for output logging

--- wordsmithAPI.py
+++ wordsmithAPI.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import PlainTextResponse
 import subprocess
 import asyncio
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -15,6 +16,19 @@ app.mount("/templates", StaticFiles(directory="templates"), name="static")  # se
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.post("/send_message")
+def send_message(message: dict):
+    user_message = message.get('message')
+
+    if user_message:
+        try:
+            with open("hi.txt", "w") as file:
+                file.write(user_message)
+            return JSONResponse(content={"message": "Message saved successfully"})
+        except Exception as e:
+            return JSONResponse(content={"error": f"Error saving message: {str(e)}"}, status_code=500)
+    else:
+        raise HTTPException(status_code=400, detail="No message provided")
 
 @app.post("/execute")
 def execute_code():
@@ -42,10 +56,30 @@ def execute_code():
 
 
 @app.get("/get_bot_responses")
-def get_bot_responses():
+def get_bot_response_with_largest_number():
     try:
-        with open("bot_responses.txt", "r") as file:
-            responses = file.read()
-            return PlainTextResponse(content=responses) #read as text
-    except FileNotFoundError:
-        return JSONResponse(content={"error": "bot_responses.txt not found"}, status_code=404)
+        base_filename = "bot_responses"
+        response_extension = ".txt"
+        
+        largest_number = None
+        largest_filename = None
+
+        for filename in os.listdir():
+            if filename.startswith(base_filename) and filename.endswith(response_extension):
+                try:
+                    number = int(filename[len(base_filename):-len(response_extension)])
+                    if largest_number is None or number > largest_number:
+                        largest_number = number
+                        largest_filename = filename
+                except ValueError:
+                    pass  # Skip files that don't match the naming pattern
+        
+        if largest_filename:
+            with open(largest_filename, "r") as file:
+                response_text = file.read()
+                return PlainTextResponse(content=response_text)
+        else:
+            return JSONResponse(content={"message": "No bot response found"}, status_code=404)
+            
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
